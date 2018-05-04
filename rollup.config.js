fd2794f1b59ada118ea8856d6090f1f484535db6
vanch3d <nicolas.github@calques3d.org>
2018-05-04 09:42:23 +0100

    updated dependencies updated scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 export default {
     input: "index",
+    external: ['d3-tip'],
     output: {
         extend: true,
+        globals: {'d3-tip': 'd3'},
         file: "dist/d3-asterPlot.js",
         format: "umd",
         name: "d3"
